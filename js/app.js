2eb351f715a8369067f4cfa8968f38c511bf0856
donriddo <donriddo@gmail.com>
2015-10-06 12:59:47 +0100

    made few changes

--- js/app.js
+++ js/app.js
@@ -1,69 +1,69 @@
 angular.module('myApp', ['ngRoute'])
 .provider('Weather', function() { var apiKey = "";
-this.setApiKey = function(key) {
-  if (key) this.apiKey = key;
-};
-var apiKey = "";
-// ...
-this.getUrl = function(type, ext) {
-  return "http://api.wunderground.com/api/" + this.apiKey + "/" + type + "/q/" + ext + '.json';
-};
-this.$get = function($q, $http) {
-  var self = this;
-  return {
-    getWeatherForecast: function(city) {
-      var d = $q.defer();
-      $http({
-        method: 'GET',
-        url: self.getUrl("forecast", city),
-        cache: true
-      })
-      .success(function(data) {
-    // The wunderground API returns the
-    // object that nests the forecasts inside // the forecast.simpleforecast key
-    d.resolve(data.forecast.simpleforecast);
-    }).error(function(err) { d.reject(err);
-    });
-    return d.promise;
-  }
-}
-};
+  this.setApiKey = function(key) {
+    if (key) this.apiKey = key;
+  };
+  var apiKey = "";
+  this.getUrl = function(type, ext) {
+    return "http://api.wunderground.com/api/" + this.apiKey + "/" + type + "/q/" + ext + '.json';
+  };
+  this.$get = function($q, $http) {
+    var self = this;
+    return {
+      getWeatherForecast: function(city) {
+        var d = $q.defer();
+        $http({
+          method: 'GET',
+          url: self.getUrl("forecast", city),
+          cache: true
+        })
+        .success(function(data) {
+          // The wunderground API returns the
+          // object that nests the forecasts inside the forecast.simpleforecast key
+          d.resolve(data.forecast.simpleforecast);
+        })
+        .error(function(err) {
+          d.reject(err);
+        });
+        return d.promise;
+      }
+    }
+  };
 })
 .config(function($routeProvider, WeatherProvider) {
   WeatherProvider.setApiKey('876385ebdb3d6520');
-    $routeProvider
-    .when('/', {
-      templateUrl: 'templates/home.html',
-      controller: 'MainController'
-    })
-    .when('/settings', {
-      templateUrl: 'templates/settings.html',
-      controller: 'SettingsController'
-    })
-    .otherwise({redirectTo: '/'});
+  $routeProvider
+  .when('/', {
+    templateUrl: 'templates/home.html',
+    controller: 'MainController'
+  })
+  .when('/settings', {
+    templateUrl: 'templates/settings.html',
+    controller: 'SettingsController'
+  })
+  .otherwise({redirectTo: '/'});
 })
 .factory('UserService', function ($location) {
   var defaults = {
     location: 'autoip',
     name: "Don Riddo"
   };
-var service = { user: {},
-save: function() {
+  var service = {
+    user: {},
+    save: function() {
       sessionStorage.momentum = angular.toJson(service.user);
       $location.path('/');
-},
-restore: function() {
-      // Pull from sessionStorage
-      service.user =
-        angular.fromJson(sessionStorage.momentum) || defaults
-        return service.user; }
-};
-// Immediately call restore from the session storage // so we have our user data available immediately
-service.restore();
-return service;
+    },
+    restore: function() {
+      service.user = angular.fromJson(sessionStorage.momentum) || defaults;
+      return service.user;
+    }
+  };
+  // Immediately call restore from the session storage so we have our user data available immediately
+  service.restore();
+  return service;
 })
 .controller('MainController', function($scope, $timeout, Weather, UserService) {
-  // Build the date object
   $scope.date = {};
 
   $scope.user = UserService.user;
@@ -77,21 +77,19 @@ return service;
       return "Good Evening " + $scope.user.name;
     }
   };
-var updateTime = function() {
-  $scope.date.raw = new Date();
-  $scope.date.greeting = greeting();
-  $timeout(updateTime, 1000);
-}
-  // Kick off the update function
+  var updateTime = function() {
+    $scope.date.raw = new Date();
+    $scope.date.greeting = greeting();
+    $timeout(updateTime, 1000);
+  }
   updateTime();
-
   $scope.weather = {}
-$scope.user = UserService.user;
-Weather.getWeatherForecast($scope.user.location)
-.then(function(data) {
-  $scope.user.city = data.forecastday[0].date.tz_long.split('/')[1];
-  $scope.weather.forecast = data;
-});
+  $scope.user = UserService.user;
+  Weather.getWeatherForecast($scope.user.location)
+  .then(function(data) {
+    $scope.user.city = data.forecastday[0].date.tz_long.split('/')[1];
+    $scope.weather.forecast = data;
+  });
 })
 .controller('SettingsController', function ($scope, UserService) {
    $scope.user = UserService.user;
